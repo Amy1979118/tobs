8ec5e96b30cb1cd002d82217d1ee9e79466e85d3
Paulo Yamabe <paulo.yamabe@gmail.com>
2019-12-19 10:31:49 -0300

    Remove excessive printing

--- stokes_tobs.py
+++ stokes_tobs.py
@@ -32,14 +32,12 @@ class Optimizer(object):
     cst_num = 0
     rho = None
 
-    def __init__(self, fc_xi):
-        #print('\tInitializing Object')
+    def __init__(self, fc_xi):        
         self.fc_xi = Function( fc_xi.function_space(), name="Control" )
         self.nvars = len(self.fc_xi.vector())
         self.control = Control(fc_xi)
 
-    def __check_ds_vars__(self):
-        #print('\t\tCheck')
+    def __check_ds_vars__(self):        
         chk_var = False
         if self.xi_array is None:
             self.xi_array = np.copy(self.rho)
@@ -55,11 +53,9 @@ class Optimizer(object):
         else:
             pass
         ds_vars = self.fc_xi        
-        #print('\t\tCheck OK')
         return ds_vars
 
-    def __vf_fun_var_assem__(self):
-        #print('\tVolume Summation')        
+    def __vf_fun_var_assem__(self):             
         fc_xi_tst   = TestFunction(self.fc_xi.function_space())
         self.vol_xi  = assemble(fc_xi_tst * Constant(1.0) * dx)
         self.vol_sum = self.vol_xi.sum()
@@ -70,19 +66,16 @@ class Optimizer(object):
     def add_objfun(self, AD_Obj_fx):
         self.objfun_rf = ReducedFunctional(AD_Obj_fx, self.control)
 
-    def obj_fun(self, user_data=None):
-        #print('\tobj_fun')
-        ds_vars = self.__check_ds_vars__()
-        #print('\tobj_fun_eval')
-        fval = self.objfun_rf(ds_vars) # slow
-#        print(" fval: ", fval)
+    def obj_fun(self, user_data=None):        
+        ds_vars = self.__check_ds_vars__()        
+        fval = self.objfun_rf(ds_vars) # slow because is a solve
         self.iter_fobj += 1
         return fval
 
     def obj_dfun(self, user_data=None):        
         ds_vars  = self.__check_ds_vars__()
         print('\tRecalculate Objective Function')
-        self.objfun_rf(ds_vars) # slow
+        self.objfun_rf(ds_vars) # slow because is a solve
         #Derivada da funcao objetivo
         print('\tEvaluating Sensibility')
         dfval = self.objfun_rf.derivative().vector()
@@ -93,21 +86,18 @@ class Optimizer(object):
         self.iter_dobj += 1
         return dfval
 
-    def add_volf_constraint(self, upp, lwr):
-        #print('\tSet Volume Constraint')
+    def add_volf_constraint(self, upp, lwr):        
         self.__vf_fun_var_assem__()
         self.cst_U.append(upp)
         self.cst_L.append(lwr)
         self.cst_num += 1
 
-    def volfrac_fun(self):
-        #print('\tVolume Fraction Function')
+    def volfrac_fun(self):        
         self.__check_ds_vars__()
         volume_val = float( self.vol_xi.inner( self.fc_xi.vector() ) )
         return volume_val/self.vol_sum
 
-    def volfrac_dfun(self, user_data=None):
-        #print('\tVolume Fraction Dfunction')
+    def volfrac_dfun(self, user_data=None):        
         v_df = self.vol_xi/self.vol_sum
         return v_df
 
@@ -116,28 +106,25 @@ class Optimizer(object):
         for i in range(self.cst_num):
             rows += [i] * self.nvars
         cols = range(self.nvars) * self.cst_num
-
         return (np.array(rows, dtype=np.int), np.array(cols, dtype=np.int))
 
-    def cst_fval(self, user_data=None):
-        #print('\tConstraint Fval')
+    def cst_fval(self, user_data=None):        
         cst_val = np.array(self.volfrac_fun(), dtype=np.float)
         return cst_val.T
 
-    def jacobian(self, flag=False, user_data=None):
-        #print('\tJacobian')
+    def jacobian(self, flag=False, user_data=None):        
         if flag:
             dfval = self.flag_jacobian()
         else:
             dfval = self.volfrac_dfun()
-
         return dfval
 
 octave.addpath('~')
 pasta = "output/"
 
+#%% Material parameters
 mu = Constant(1.0)                   # viscosity
-#alphaunderbar = 2.5 * mu / (100**2)  # parameter for \alpha
+#alphaunderbar = 2.5 * mu / (100**2)  # parameter for \alpha 
 #alphabar = 2.5 * mu / (0.01**2)      # parameter for \alpha
 alphaunderbar = 0.0
 alphabar = 1.0e5
@@ -148,7 +135,7 @@ def alpha(rho):
     return Constant(alphabar) + (Constant(alphaunderbar) - Constant(alphabar)) * rho * (1.0 + Constant(q)) / (rho + Constant(q))
 
 #%% Mesh
-N = 15
+N = 14
 delta = 1.5  # The aspect ratio of the domain, 1 high and \delta wide
 
 #mesh = Mesh(RectangleMesh(Point(0.0, 0.0), Point(delta, 1.0), int(15*N), int(10*N), diagonal="crossed"))
@@ -158,11 +145,9 @@ mesh = RectangleMesh.create([Point(0.0,0.0),Point(delta,1.0)], [int(15*N+1),int(
 mesh = Mesh(mesh)
 
 A = FunctionSpace(mesh, "DG", 0) # control function space
-
 U_h = VectorElement("CG", mesh.ufl_cell(), 2)
 P_h = FiniteElement("CG", mesh.ufl_cell(), 1)
 W = FunctionSpace(mesh, U_h*P_h)          # mixed Taylor-Hood function space
-
 # Define the boundary condition on velocity
 class InflowOutflow(UserExpression):
     def eval(self, values, x):
@@ -170,7 +155,6 @@ class InflowOutflow(UserExpression):
         values[0] = 0.0
         l = 1.0/6.0
         gbar = 1.0
-
         if x[0] == 0.0 or x[0] == delta:
             if (1.0/4 - l/2) < x[1] < (1.0/4 + l/2):
                 t = x[1] - 1.0/4
@@ -178,7 +162,7 @@ class InflowOutflow(UserExpression):
             if (3.0/4 - l/2) < x[1] < (3.0/4 + l/2):
                 t = x[1] - 3.0/4
                 values[0] = gbar*(1 - (2*t/l)**2)
-
+    
     def value_shape(self):
         return (2,)
 
@@ -192,7 +176,6 @@ def forward(rho):
          inner(grad(p), v) * dx  + inner(div(u), q) * dx)
     bc = DirichletBC(W.sub(0), InflowOutflow(degree=1), "on_boundary")
     solve(lhs(F) == rhs(F), w_resp, bcs=bc, solver_parameters={'linear_solver':'mumps'})
-
     return w_resp
 
 class Distribution(UserExpression):
@@ -207,18 +190,17 @@ class Distribution(UserExpression):
 
 def cplex_optimize(prob, nvar, my_obj, my_constcoef, my_rlimits, my_ll, my_ul):
     prob.objective.set_sense(prob.objective.sense.minimize)
-
     my_ctype = "I"*nvar
     my_colnames = ["x"+str(item) for item in range(nvar)]
     my_sense = ["L", "G"]
     my_rownames = ["r1", "r2"]
-
+    
     prob.variables.add(obj=my_obj, lb=my_ll, ub=my_ul, types=my_ctype,
                        names=my_colnames)
-
+    
     rows = [cplex.SparsePair(ind=["x"+str(item) for item in range(nvar)], val = my_constcoef[0]),
             cplex.SparsePair(ind=["x"+str(item) for item in range(nvar)], val = my_constcoef[1])]
-
+    
     prob.linear_constraints.add(lin_expr=rows, senses=my_sense, rhs=my_rlimits, names=my_rownames)
 
 def helmholtz_filter(phi):
@@ -264,64 +246,45 @@ def helmholtz_filter(phi):
 if __name__ == "__main__":
     controls = File(pasta + "control.pvd")
     state_file = File(pasta + "velocity.pvd")
-
+    # Set initial guess
     rho = interpolate(Distribution(), A)
     rho.rename("control", "")
-    controls << rho
-
-#    print ('\tForward Problem')
-#    w_resp   = forward(rho)
-#    (u, p) = w_resp.split()
-#    u.rename("u", "")
-#    state_file << u
-
     iteration = 0
+    max_iter = 100
     epsilons = 0.025
 
     print('\tStart Optimization Loop')
     #%% Opt Loop
-    while True:
-        set_working_tape(Tape()) # Create new Tape for Adjoint
-        rho.rename("control", "")
-        controls << rho
-
+    opt_convergence = False
+    while opt_convergence is False:
+        set_working_tape(Tape()) # Create new Tape for Adjoint        
         print ('\tForward Problem')
         w_resp   = forward(rho)
         (u, p) = w_resp.split()
         u.rename("velocity", "")
-        state_file << u
-        
-        print('\tCreate Opt object')
-        fval = Optimizer(rho)
-        print('\tGet design Variable')
+        rho.rename("control", "")
+        print('\tSaving Files')
+        state_file << u        
+        controls << rho
+        # Create Opt Object
+        fval = Optimizer(rho)        
         fval.rho = rho.vector()
         nvar = len(rho.vector())
-        
-        print('\tObjective Function Assemble')
+        # Objective Funcion
         J = assemble( (0.5 * inner(alpha(rho) * u, u)
-                    + 0.5 * mu * inner(grad(u)+grad(u).T, grad(u)+grad(u).T) )* dx)
-        #print('\tSet Objective Function')
+                    + 0.5 * mu * inner(grad(u)+grad(u).T, grad(u)+grad(u).T) )* dx)        
         fval.add_objfun(J)
 #        j = float(fval.obj_fun(rho.vector()))
-        j = float(J)
-        
-        print('\tPreparing Sensibility')
-        if iteration == 0: jd_previous = np.array(fval.obj_dfun()).reshape((-1,1))
+        j = float(J)        
+#        if iteration == 0: jd_previous = np.array(fval.obj_dfun()).reshape((-1,1))
 #        jd = (np.array(fval.obj_dfun()).reshape((-1,1)) + jd_previous)/2 #stabilization
         jd = np.array(fval.obj_dfun()).reshape((-1,1)) # No stabilization        
-#        fid_rho = File("rho.pvd")
-        
-        #print('\tSet Constraints Limits')
         fval.add_volf_constraint(0.7,0.5)
         x_L = np.ones((nvar), dtype=np.float) * 0.0
         x_U = np.ones((nvar), dtype=np.float) * 1.0
         acst_L = np.array(fval.cst_L)
-        acst_U = np.array(fval.cst_U)
-        
-        #print('\tSet Constraints')
-        cs = fval.cst_fval()
-        
-        #print('\tSet Constraints Jacobian')
+        acst_U = np.array(fval.cst_U)        
+        cs = fval.cst_fval()        
         jac = np.array(fval.jacobian()).reshape((-1,1))
 
         print ('\n\tIter: %3.d \tObj Func: %.4f \tVol Frac: %.4f' %(iteration, j, fval.volfrac_fun()))
@@ -341,6 +304,8 @@ if __name__ == "__main__":
                 epsilons,
                 np.array(rho.vector())
                 )
+        
+        # Cplex variables
         PythonObjCoeff = ans[0][1] #because [0][0] is the design variable
         PythonConstCoeff = ans[0][2]
         PythonRelaxedLimits = ans[0][3]
@@ -350,9 +315,9 @@ if __name__ == "__main__":
         my_prob = cplex.Cplex()
 
         # cplex log
-        my_prob.set_log_stream(None)
-        my_prob.set_error_stream(None)
-        my_prob.set_warning_stream(None)
+        #my_prob.set_log_stream(None)
+        #my_prob.set_error_stream(None)
+        #my_prob.set_warning_stream(None)
         my_prob.set_results_stream(None)
         
         # cplex parameters
@@ -371,18 +336,18 @@ if __name__ == "__main__":
         rho.rename("control", "")
         print('\tUpdate Design Variables')
         rho.vector().add_local(np.array(design_variables))
-#        controls << rho
 
-#        print('\tUpdate Velocity')
-#        w_resp   = forward(rho)
-#        (u, p) = w_resp.split()
-#        u.rename("u", "")
-#        state_file << u
-
-        if iteration >= 100:
-            break
+        if iteration >= max_iter:
+            opt_convergence = True
         else: iteration += 1
         jd_previous = jd
         print('\n----------------------------------------')
-#        del my_prob
 
+    print ('\tForward Problem')
+    w_resp   = forward(rho)
+    (u, p) = w_resp.split()
+    u.rename("velocity", "")
+    rho.rename("control", "")
+    print('\tSaving Last Solution')
+    state_file << u        
+    controls << rho
